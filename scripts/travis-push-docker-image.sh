2168d9d641479cacface547224bbeb8e86e572c4
linzhaoming <teleyic@gmail.com>
2019-06-05 17:30:54 +0800

    Travis

--- scripts/travis-push-docker-image.sh
+++ scripts/travis-push-docker-image.sh
@@ -8,9 +8,12 @@ set -u
 
 export REPO="logancloud/logan-app-operator"
 
+echo "Pushing to docker hub ${REPO}:${TAG}"
+
 if [[ "${TRAVIS_TAG}" != "" ]]; then
 	# For both git tags and git branches 'TRAVIS_BRANCH' contains the name.
     export TAG="${TRAVIS_BRANCH}"
     docker tag ${REPO}:latest "${REPO}:${TAG}"
+    echo "Pushing to docker hub ${REPO}:${TAG}"
     docker push "${REPO}:${TAG}"
 fi
