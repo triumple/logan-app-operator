4fc5e429203a8a4a03aa3c729809efc35d2a80dc
linzhaoming <teleyic@gmail.com>
2019-06-18 14:29:18 +0800

    travis: poc of PR-3

--- scripts/travis-push-docker-image.sh
+++ scripts/travis-push-docker-image.sh
@@ -8,6 +8,11 @@ set -u
 
 export REPO="logancloud/logan-app-operator"
 
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    docker push ${IMG}
+fi
+
 if [[ "${TRAVIS_TAG}" != "" ]]; then
 	# For both git tags and git branches 'TRAVIS_BRANCH' contains the name.
     export TAG="${TRAVIS_BRANCH}"
