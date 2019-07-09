f05f08eb3dc1289bccbc2fc1543c9c88c28ef693
triumple <triumple163@163.com>
2019-07-09 14:38:55 +0800

    travis test 28

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ echo $now
 echo $TRAVIS_SD_START_TIME
 echo $elapsed_nano
 elapsed_time=`expr $elapsed_nano / 1000000000`
-error_info="Build #$TRAVIS_BUILD_NUMBER($TRAVIS_COMMIT) of $TRAVIS_REPO_SLUG@$TRAVIS_BRANCH fail in $elapsed_time seconds"
+error_info="Build #$TRAVIS_BUILD_NUMBER($TRAVIS_COMMIT) of $TRAVIS_REPO_SLUG@$TRAVIS_BRANCH $TRAVIS_SD_STATUS in $elapsed_time seconds"
 msg="{\"msgtype\": \"text\", \"text\": { \"content\": \"failure, reason: $error_info\" }}"
 echo $msg
 curl 'https://oapi.dingtalk.com/robot/send?access_token=5349ab8e8a4ec739e5f2c704ebdcf50bf1e52c5b925d176a871c4e5b98735a20' \
