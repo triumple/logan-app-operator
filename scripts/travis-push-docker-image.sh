56c57897c4d3b0797383089185332a3b548ddcfb
triumple <triumple163@163.com>
2019-07-09 15:43:55 +0800

    re test 2

--- scripts/travis-push-docker-image.sh
+++ scripts/travis-push-docker-image.sh
@@ -18,7 +18,10 @@ gitlogs="$(git log -1 | grep "Merge pull request")"
 gitlogs="Merge pull request #44 from triumple/operator_timezone_fix"
 re="Merge pull request #([0-9]+) .*"
 if [[ $gitlogs =~ $re ]]; then
-    pullRequstId=${BASH_REMATCH[1]};
+    echo "3"
+    echo ${BASH_REMATCH[1]}
+    pullRequstId=${BASH_REMATCH[1]}
+    echo "4"
 fi
 
 echo $pullRequstId
